c17b3ab378f7ef271c7b1c27d852b5b93fa8905d
himadri116 <himadribahuguna8@gmail.com>
2026-02-21 22:03:25 +0530

    Final stable MERN auth with Docker and Mongo fix

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+// ✅ AUTH ROUTES
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
